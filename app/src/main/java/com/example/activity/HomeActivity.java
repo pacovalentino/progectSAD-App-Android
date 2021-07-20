31a76fdff6e10b29cda8a38eeb8f0c947a46bf5f
Fabio <fa.paduano@studenti.unina.it>
2021-07-20 19:00:30 +0200

    + traduzione italiano caricamento

--- app/src/main/java/com/example/activity/HomeActivity.java
+++ app/src/main/java/com/example/activity/HomeActivity.java
@@ -32,7 +32,7 @@ public class HomeActivity extends AppCompatActivity {
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Nessuna Notifica", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
 
